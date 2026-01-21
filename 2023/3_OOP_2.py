a328743f1527f2ed6720a4501d1e111c215f00ae
Dāniels Strauts <daniels.strauts@ventspils.edu.lv>
2026-01-21 16:11:29 +0200

    Cina ar 23 gada 3 dalas 2. uzd

--- 2023/3_OOP_2.py
+++ 2023/3_OOP_2.py
@@ -6,43 +6,71 @@ class kubs:
         try:
             if 2<= self.mGarums <=10:
                 print(self.mGarums)#self.mGarums = mGarums
+
             else:
-                print("neatbilst intervālam")
+                if self.mGarums < 2 :
+                    self.mGarums = 2
+                elif self.mGarums > 10:
+                    self.mGarums = 10
 
-        except:
-            print("ex")
+            if len(self.krasa.split()) != 1 and self.krasa.isalpha() == True:
+                print("nav krasa uzrakstīta vienā vārdā")
+
+        except Exception as e:
+            print(e)
             
-            pass
 
-    # def __init__(self):
-    #     self.mGarums = 0 
-    #     self.krasa = ""
+    def aprekinat_tilpumu(self):
+        tilpums = self.mGarums ** 3
+        return tilpums
     
-    #     while True:
-    #         try:
-    #             i1 = int(input("ievadi kuba malas garumu (malas atļautais garums no 2 līdz 10 ieskaitot)"))
-    #             if 2 <= i1 <= 10:
-    #                 self.mGarums = i1
-    #                 break
-    #             else:
-    #                 print("Garums neiekļaujas intervālā")
-    #                 continue
-    #         except:
-    #             print("Notikusi kļūda")
-    #             continue
+    def __del__(self):
+        pass
+ 
 
-    #     while True:
-    #         try:
-    #             i2 = str(input("Ievadi krāsas nosaukumu (viens vārds) - "))
-    #             if len(i2.split()) == 1 and i2.isalpha() == True:
-    #                 self.krasa = i2
-    #                 break
-    #             else:
-    #                 print("Krāsa nav uzrakstīta vispār vai arī ir par 1 vārdu vairāk")
-    #                 continue
-                
-    #         except:
-    #             print("Notikusi kļūda")
-    #             break
+kubg=kubs(10,"zaļa")
+kubr=kubs(1,"sarkans")
+print(f"{kubg.krasa,kubg.aprekinat_tilpumu()} cm3")
+print(kubr.mGarums)
+del kubr
+try:
+    print(kubr.krasa)
+    print("nav izdzests")
 
-kubg=kubs(2,"eqw")
+except Exception as e :
+    print(f"Izdzēsts {e}")
+print(kubg.mGarums)
+
+class bloks(kubs):
+
+    def __init__(self,kubuSk,forma):
+        # self.__kubuSk = kubuSk
+        # self.nosaukums = nosaukums
+        # self.forma = forma
+        # self.derigums = derigums
+        
+
+        try:
+            if 11>= int(forma) <=14 or int(forma) == 22:
+                self.derigums = 1 
+            else:
+                self.derigums = 0
+
+            if 1>=int(kubuSk) <= 4:
+                self.kubuSk = kubuSk
+            else:
+                print("Kubu skaits neatbilst intervālam")
+        except Exception as e:
+            print(f"Kļūda: {e}")
+
+        self.nosaukums = self.krasa,self.kubuSk
+
+    def tilpums(self):
+        blokTilpums = self.kubuSk * self.mGarums**3
+        print(blokTilpums)
+
+
+o1.bloks(5,"oranžs",3,13)
+
+
+                 
